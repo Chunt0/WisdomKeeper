64c1410b0fd9ef705728677016be58cb9d235747
Chunt0 <chunt.mail@gmail.com>
2023-08-12 09:50:06 -0700

    2023-08-12 09:50:06

--- web-app/app.py
+++ web-app/app.py
@@ -5,27 +5,29 @@ from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 def get_questions():
+    recorded = []
     questions = []
     with open('../dataset/data.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
-            questions.append((row[0],row[1]))
-    return questions
+            recorded.append(row[0])
+            questions.append(row[1])
+    return recorded, questions
 
 def update_question(index):
     # Read the CSV file into a DataFrame
     df = pd.read_csv('../dataset/data.csv')
 
     # Update the specific cell
-    df.at[index, 'column_name'] = 1 # Replace 'column_name' with the name of the column you want to modify
+    df.at[index-1, 'recorded'] = 1 # Replace 'column_name' with the name of the column you want to modify
 
     # Write the DataFrame back to the CSV file
     df.to_csv('../dataset/data.csv', index=False)
 
 @app.route('/')
 def index():
-    questions = get_questions()
-    return render_template('index.html', questions=questions)
+    recorded, questions = get_questions()
+    return render_template('index.html', recorded=recorded, questions=questions)
 
 @app.route('/update_question', methods=['POST'])
 def update():
